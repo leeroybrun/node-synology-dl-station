c0bbcf0b29fee2567ca5f76604e6aa1df0b5355d
unknown <leeroy.brun@POSTE-LEEROY.batiplus.local>
2013-05-23 11:52:20 +0200

    Base structure

---
+++ examples/addTask.js
@@ -0,0 +1,11 @@
+var Synology = require('../lib/synology-dl-station');
+
+var synology = new Synology('****', '****', '****');
+
+/*synology.addTask(function(error) {
+	if(!error) {
+		console.log('Success !');
+	} else {
+		console.log('Error !');
+	}
+})*/
